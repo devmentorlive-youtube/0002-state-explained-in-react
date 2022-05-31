f64d7de4ee2d3977599726fa4a7ca4970e5943a2
Mark Tellez <mark@devmentor.live>
2022-05-31 16:52:01 -0500

    toolbar open/close

--- src/features/toolbar/index.jsx
+++ src/features/toolbar/index.jsx
@@ -1,20 +1,60 @@
+
+import { useState } from "react";
 import Tool from "./tool";
 
+
 export default function Toolbar({}) {
+  const [open, setOpen] = useState(true);
+
   return (
-    <div className="fixed left-0 top-0 bottom-0 w-[260px] bg-gray-900 text-gray-50 h-screen">
-      <div className="w-full h-screen flex flex-col items-center gap-12">
-        <div className="uppercase font-black text-xl mt-24 px-8 w-full">
-          Toolbar
+    <div
+      className="fixed left-0 top-0 bottom-0 bg-gray-900 text-gray-50 h-screen z-50"
+      style={{ width: open ? 250 : 50 }}>
+      <div className="w-full flex flex-row-reverse h-4">
+        <div
+          className="rounded-full w-4 h-4 bg-gray-800 m-4 cursor-pointer"
+          onClick={() => setOpen((prev) => !prev)}
+        />
+      </div>
+      {open ? (
+        <div className="w-full h-screen flex flex-col items-center gap-12 p-8">
+          <div className="uppercase font-black text-xl mt-24 px-8 w-full">
+            Toolbar
+          </div>
+          <div className="grid grid-cols-2 gap-2">
+            {["top left", "top right", "bottom left", "bottom right"].map(
+              (name) => (
+                <Tool name={name} active={name === "bottom left"} />
+              )
+            )}
+          </div>
         </div>
-        <div className="grid grid-cols-2 gap-2">
+      ) : (
+        <div className="p-2 flex flex-col gap-2 mt-8">
           {["top left", "top right", "bottom left", "bottom right"].map(
             (name) => (
-              <Tool name={name} active={name === "bottom left"} />
+              <Tool small name={name} active={name === "bottom left"} />
             )
           )}
         </div>
-      </div>
+      )}
     </div>
   );
 }
+<<<<<<< HEAD
+=======
+
+function Tool({ name, small = false, active = false, onClick = () => {} }) {
+  return (
+    <div
+      onClick={onClick}
+      className={`${
+        active ? "bg-blue-500 border-blue-400" : "border-gray-700"
+      } border ${
+        small ? "w-8 h-8" : "w-24 h-24"
+      }  rounded-xl flex items-center justify-center`}>
+      {name.split(" ").map((w) => w[0])}
+    </div>
+  );
+}
+>>>>>>> toolbar open/close
